89862e08186e22468d99a6d5485d36c72175d517
Russel Kurt Nolasco <kurtlonolasco@gmail.com>
2024-01-10 17:49:39 +0800

    first commit

---
+++ resources/views/listings.blade.php
@@ -0,0 +1,12 @@
+<h1>{{$heading}}</h1>
+@foreach ($listings as $listing)
+    <h2>
+        <a href="listings/{{$listing['id']}}">
+            {{$listing['title']}}
+        </a>
+    </h2>
+    <p>{{$listing['description']}}</p>
+    <script>
+        console.info("Listing ID: {{$listing['id']}}");
+    </script>
+@endforeach
